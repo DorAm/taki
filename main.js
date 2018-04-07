438f3e55b3247f64f42c0cf59c50d8e9bf469cb4
Dor Amar <damar@paloaltonetworks.com>
2018-04-07 16:50:21 +0300

    wip

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-var numOfPlayers = 2;
 var gGameState = {
     board: {
         deck: [],
@@ -16,54 +15,66 @@ var gGameState = {
 };
 
 function initGame() {
-    var menu = document.querySelector('.menu');
+    var menu = document.querySelector('.main-menu');
     menu.style.display = 'none';
     var gameArena = document.querySelector('.game-arena');
-    gameArena.style.visibility = 'visible';
+    gameArena.style.display = 'block';
+    // Deck
     gGameState.board.deck = createDeck();
     shuffleDeck(gGameState.board.deck);
-    gGameState.players = createPlayers();
+    gGameState.board.deck.forEach(function (card) {
+        createCard(card);
+    });
 
-    dealHands();
-    // dealing the pile the first card of the game
-    gGameState.board.pile.push(gGameState.board.deck.pop());
-    //deciding which player starts the game
-    pickFirstPlayer();
-    pickNextPlayer();
+    // Players:
+    createPlayers();
+
     startGame();
 }
+
+
+// function toggleDisplay(selector) {
+//     debugger;
+//     var element = document.querySelector(selector);
+//     console.log(selector);
+//     console.log('element.style.display = ', element.style.display);
+//     element.style.display = (element.style.display === 'none') ? 'block' : 'none';
+// }
 
 //each turn we need to check : if ( isDeckEmpty() ) restockDeck();
 
 
-
 // card constructor
-function CardConstructor(color, number, action) {
-    this.color  = color;
+function Card(color, number, action) {
+    this.color = color;
     this.number = number;
     this.action = action;
 }
 
 // player constructor
-function PlayerConstructor(name) {
+function Player(name) {
     this.name = name;
     this.hand = [];
     this.singleCardCounter = 0;
 }
 
 function dealHands() {
-    //dealing 8 cards to each player
     for (var i = 1; i <= 8; i++) {
-        for (var j = 0; j < numOfPlayers; j++) {
+        gGameState.players.forEach(function (player) {
             var card = gGameState.board.deck.pop();
-            gGameState.players[j].hand.push(card);
-        }
+            player.hand.push(card);
+        });
     }
 }
 
 function startGame() {
-
+    dealHands();
+    // dealing the pile the first card of the game
+    gGameState.board.pile.push(gGameState.board.deck.pop());
+    //deciding which player starts the game
+    pickFirstPlayer();
 }
+
 function shuffleDeck(deckOfCards) {
     var j, x, i;
     for (i = deckOfCards.length - 1; i > 0; i--) {
@@ -75,10 +86,14 @@ function shuffleDeck(deckOfCards) {
 }
 
 function createPlayers() {
-    var players = [] ;
-    players.push(new PlayerConstructor("Amit"));
-    players.push(new PlayerConstructor("Computer"));
-    return players;
+    // var numOfPlayers = prompt('Pick the number of players');
+    var players = [];
+    // for (var i = 0; i < numOfPlayers; i++) {
+    //     players.push(new Player(prompt('Enter player' + i + 's name:')));
+    // }
+    players.push(new Player('Amit Hagever'));
+    players.push(new Player('Computer'));
+    gGameState.players = players;
 }
 
 function createDeck() {
@@ -89,76 +104,66 @@ function createDeck() {
         }
         for (var j = 1; j <= 2; j++) {
             for (var color in ColorEnum) {
-                deck.push(new CardConstructor(ColorEnum[color], CardNumberEnum[number], null));
+                deck.push(new Card(ColorEnum[color], CardNumberEnum[number], null));
             }
         }
     }
     for (var action in ActionCardEnum) {
-        if (action !== 'ChangeColor' ) {
+        if (action !== ActionCardEnum.ChangeColor) {
             for (j = 1; j <= 2; j++) {
 
                 for (color in ColorEnum) {
-                    deck.push(new CardConstructor(ColorEnum[color], null, ActionCardEnum[action]));
+                    deck.push(new Card(ColorEnum[color], null, ActionCardEnum[action]));
                 }
             }
-        } else if (action === 'ChangeColor') {
+        } else if (action === ActionCardEnum.ChangeColor) {
             for (j = 1; j <= 4; j++) {
-                deck.push(new CardConstructor(null, null, ActionCardEnum[action]));
+                deck.push(new Card(null, null, ActionCardEnum[action]));
             }
         }
     }
-    shuffleDeck(deck);
     return deck;
 }
 
-function pickFirstPlayer()  {
-//     TODO : sophisticate the process
-    gGameState.currentPlayer(gGameState.players[0]);
+function pickFirstPlayer() {
+    var randomNumber = getRandomInt(0, gGameState.players.length);
+    gGameState.currentPlayer = gGameState.players[randomNumber];
 }
 
-function pickNextPlayer()  {
-    var index = gGameState.players.indexOf(gGameState.currentPlayer );
-
-    if (  gGameState.board.direction === 'CLOCKWIZE' ) {
-
-        // when current player is last in array and board direction is clockWize
-        if (index === (gGameState.players.length - 1)) {
-            gGameState.currentPlayer = gGameState.players[0];
-        } else {
-            gGameState.currentPlayer = gGameState.players[index + 1];
-        }
-    } else if (gGameState.board.direction === 'COUNTERCLOCK' ) {
-
-        // when current player is first in array and board direction is counter Clock
-        if ( (index) ===  0 ) {
-            gGameState.currentPlayer = gGameState.players[gGameState.players.length - 1];
-        } else {
-            gGameState.currentPlayer = gGameState.players[index - 1];
-        }
-    }
+function pickNextPlayer() {
+    var currentPlayerIndex = gGameState.players.indexOf(gGameState.currentPlayer);
+    var nextPlayerIndex = (currentPlayerIndex + isClockwize() ? 1 : -1) % gGameState.players.length;
+    gGameState.currentPlayer = gGameState.players[nextPlayerIndex];
 }
+
+function isClockwize() {
+    return gGameState.board.direction === 'CLOCKWIZE';
+}
+
 // BOT rules of playing
-function botMind() {
+// function botMind() {
+//
+//     var lCard = gGameState.board.leadingCard;
+//
+//     if ((lCard.color number === 2
+// ) &&
+//     (player.hand.find(card.number))
+//
+// }
 
-    var lCard = gGameState.board.leadingCard;
 
-    if ( (lCard.color number === 2) && ( player.hand.find(card.number))
-
+function isDeckEmpty() {
+    return gGameState.board.deck.length === 0;
 }
 
-
-function isDeckEmpty(deckOfCards) {
-    if ( deckOfCards.length() === 0 ) )
-        return true;
-    else
-        return false;
-}
 function restockDeck() {
-    while ( gGameState.board.pile.length > 1 ) {
+    // The leading card must stay in the pile
+    while (gGameState.board.pile.length > 1) {
         gGameState.board.deck.unshift(gGameState.board.pile.shift());
     }
     shuffleDeck(gGameState.board.deck);
 }
+
 /*
 function renderDeck() {
     var htmlStr = '';
@@ -167,5 +172,49 @@ function renderDeck() {
     });
     document.querySelector('.hand').innerHTML = htmlStr;
 }
-
 */
+
+function createCard(card) {
+    var cardOutline = document.createElement('div');
+    cardOutline.classList.add('cardOutline');
+    var cardTop = document.createElement('div');
+    cardTop.classList.add('cardTop');
+    var cardCenter = document.createElement('div');
+    cardCenter.classList.add('cardCenter');
+    var cardBottom = document.createElement('div');
+    cardBottom.classList.add('cardBottom');
+    cardOutline.appendChild(cardTop);
+    cardOutline.appendChild(cardCenter);
+    cardOutline.appendChild(cardBottom);
+
+
+    for (var i = 1; i <= 2; i++) {
+        var icon = document.createElement('div');
+        var text = document.createTextNode(card.number);
+        icon.style.color = card.color;
+        icon.appendChild(text);
+        cardTop.appendChild(icon);
+    }
+
+    var icon = document.createElement('div');
+    var text = document.createTextNode(card.number);
+    var icon = document.createElement('div');
+
+    icon.appendChild(text);
+    cardCenter.appendChild(icon);
+
+    for (var i = 1; i <= 2; i++) {
+        icon.style.color = card.color;
+        var icon = document.createElement('div');
+        var text = document.createTextNode(card.number);
+        icon.appendChild(text);
+        cardBottom.appendChild(icon);
+    }
+
+    var cardsArea = document.querySelector('.cardsArea');
+    cardsArea.appendChild(cardOutline);
+}
+
+// function cardDisplay(card) {
+//     return card.number ? card.number : card.action;
+// }
